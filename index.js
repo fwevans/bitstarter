8a631260e1d4b6fdb6545d5607a7d6f4f61e099a
fwevans <feliciayiu@gmail.com>
2015-05-08 22:02:27 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-var fs = require('fs');
-var indexcontent = fs.readFileSync('/index.html');
 
-app.set('port', (process.env.PORT || 8080))
-app.use(express.static(__dirname + '/public'))
+var fs = require('fs');
+var htmlfile = "index.html";
+
+app.set('port', (process.env.PORT || 8080));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(indexcontent.toString('utf8'))
-})
+  var html = fs.readFileSync(htmlfile).toString();
+    response.send(html)
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
