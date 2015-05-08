c0ea4b29204170ab9d82d285deb00ae4d723102f
fwevans <feliciayiu@gmail.com>
2015-05-08 21:16:12 +0000

    Uses readfilesync and new string from index.html

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var app = express();
+var fs = require('fs');
+var indexcontent = fs.readFileSync('/index.html');
 
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(indexcontent.toString('utf8'))
 })
 
 app.listen(app.get('port'), function() {
